38b4b06018e5020b0e79e9b55d9d48fb42d69c95
kristopher Greene <x00100794@gmail.com>
2014-10-14 16:37:04 +0100

    commands for github

---
+++ x00100794-grades.sh
@@ -0,0 +1,36 @@
+#TEST CASE 1
+#inputs: 10 50
+#expected output: pass
+#Actual output:pass
+java grades 80 50
+
+#TEST CASE 2
+#inputs: 40 60
+#expected output: pass
+#Actual output:fail
+java grades 40 60
+
+#TEST CASE 3
+#inputs: 80 100
+#expected output: pass with distinction
+#Actual output:pass with distinction
+java grades 80 100
+
+#TEST CASE 4
+#inputs: 20 90
+#expected output: component fail
+#Actual output: component fail
+java grades 20 90
+
+#TEST CASE 5
+#inputs: 50 80
+#expected output: pass
+#Actual output:pass
+java grades 50 80
+
+#TEST CASE 6
+#inputs: 80 81
+#expected output: pass with distinction
+#Actual output:
+java grades 80 81
+
